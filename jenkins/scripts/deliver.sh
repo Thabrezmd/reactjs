519d8c79b10787dc3f10d9bb31e995f5c2049f0c
Thabrezmd <116071169+Thabrezmd@users.noreply.github.com>
2023-04-11 14:47:34 +0530

    cc

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo 'Visit http://3.110.87.198:8080 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 8080:8080''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
